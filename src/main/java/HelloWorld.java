75e91e900c4088b42408d4d1cac4a1a6dfbe9574
Ekkarat Promrit <ekkaratpro@mathregit.group>
2020-08-03 11:41:23 +0700

    done

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    private String name;
+
+    public String sayHi() {
+        return "Hello World";
+    }
+
+    public String sayHi(String name) {
+        return "Hello " + name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase();
+    }
+}
